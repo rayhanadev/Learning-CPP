98df597a54d90acf738d2f4186e3f4968eb9bae4
Rayhan Arayilakath <rayhanadev@pm.me>
2021-03-29 11:22:44 -0500

    Create DAY_1.cpp

---
+++ DAY_1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  /* The code below will print the words Hello World!
+  to the screen, and it is amazing */
+  
+  // This is a comment
+  cout << "Hello World!\n"; // This is another comment
+  cout << "I am learning C++.\n";
+  
+  /* Now it will do magic your tiny human brains
+  cannot comprehend */
+
+  int x;
+  cout << "Type a number: ";
+  cin >> x;
+  cout << "\n";
+  cout << "I will guess your number...\n";
+  cout << "Your number is: " << x << "\n";
+  cout << "See told you I would guess it XD!\n";
+  
+  return 0;
+}
